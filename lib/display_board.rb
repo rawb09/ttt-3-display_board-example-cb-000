8ad879a9a659ef549782581e7f11d9467710a838
Robert Aguilar <aguilar.r@live.com>
2020-01-31 04:23:25 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,7 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
-display_board = "
-   |   |
------------
-   |   |
------------
-   |   |   "
+display_board =   puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
